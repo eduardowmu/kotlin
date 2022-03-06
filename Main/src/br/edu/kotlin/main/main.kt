d556541df39792ed8aefda01d9ebfcda82f64fb0
Eduardo Watanabe Murakoshi <eduardowmu@yahoo.com.br>
2022-03-05 23:54:15 -0300

    Strings

---
+++ Main/src/br/edu/kotlin/main/main.kt
@@ -0,0 +1,10 @@
+package br.edu.kotlin.main
+
+fun main() {
+    val nome = """Eduardo
+        |Watanabe
+        |Murakoshi
+    """.trimMargin()
+
+    println("Tamanho do nome vale ${nome.length}")
+}
