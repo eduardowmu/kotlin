4e073f7e4928ff9b8472a4c80a7c9fb537de7572
eduardo.murakoshi@groove.tech <eduardo.murakoshi@groove.tech>
2022-03-20 00:09:19 -0300

    Tipos numericos e conversões.

---
+++ Main/src/br/edu/kotlin/main/convert.kt
@@ -0,0 +1,11 @@
+package br.edu.kotlin.main
+fun teste(n1: Long) {}
+
+fun main() {
+    val n1: Byte = Byte.MAX_VALUE
+    val b1: Double = n1.toDouble()
+    println(n1)
+    println(b1)
+
+    println("20".toInt())
+}
